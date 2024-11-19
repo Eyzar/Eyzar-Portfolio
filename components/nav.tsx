7914a8a0982f3a8267441102494fcb426c7c2279
Eyzar Hermanio <hermanioeyzar@gmail.com>
2024-11-19 15:57:51 +0700

    Creating NextAuth API and Login Logout Function

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,15 +2,22 @@ import Link from 'next/link';
 import React from 'react'
 import ToggleMode from './ToggleMode';
 import MainNavLinks from './MainNavLink';
+import { getServerSession } from 'next-auth';
+import options from '@/app/api/auth/[...nextauth]/options';
 
 
 
-const MainNav = () => {
+const MainNav = async () => {
+
+  const session = await getServerSession(options)
   return (
     <div className='flex justify-between'>
         <MainNavLinks />
+
         <div className='flex items-center gap-4'>
-            <Link href="/login">Logout</Link>
+            {session 
+            ? <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+            : <Link href="/api/auth/signin">Login</Link>}
             <ToggleMode />
         </div>
     </div>
